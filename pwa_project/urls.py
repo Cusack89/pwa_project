2be6402c61305fee524b92e51af0104179090b5f
Francesca Cusack <francescacusack@Francescas-MacBook-Air.local>
2026-02-20 12:01:24 +1100

    Basic Auth

--- pwa_project/urls.py
+++ pwa_project/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [ 
 	path('admin/', admin.site.urls), 
+	path('users/', include(("users.urls", "users"), namespace="users")),
 	path('tasks/', include(("tasks.urls", "tasks"), namespace="tasks")),
 ]
